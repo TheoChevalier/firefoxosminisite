6f0e3c75c37747daeeb2bb01d425fa8db403834e
TheoChevalier <theo.chevalier11@gmail.com>
2014-10-18 13:20:32 +0200

    Visual refresh

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!doctype html>
 
-	<?php include('../includes/header.php'); ?>
+	<?php include('./includes/header.php'); ?>
 
         <?php include('./includes/body.php'); ?>
         <?php include('./includes/footer.php'); ?>
